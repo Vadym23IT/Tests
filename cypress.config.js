e22d6fa466c70ae86d5d6c06e2879bb319143b61
Vadym Trokhymchuk <trokhimchuk2003@gmail.com>
2024-10-01 10:27:29 +0300

    Added commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "i9azzs",
+  projectId: "i9azzzs",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "https://telnyx.com/", 
+    supportFile: "cypress/support/e2e.js",
   },
 });
